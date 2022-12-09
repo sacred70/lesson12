2a00a8e2e042e5a1796f355be5e00781ef90b8d4
sacred70 <sacred70@mail.ru>
2022-12-09 11:47:00 +0700

    1

--- main/main.py
+++ main/main.py
@@ -1,7 +1,13 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 main_blueprint = Blueprint("main_blueprint", __name__, template_folder='templates')
 
 @main_blueprint.route("/")
 def main():
     return render_template("index.html")
+
+
+@main_blueprint.route("/search/?s=<key_search>")
+def search(key_search):
+    s = request.args.get('s', "")
+    return render_template("post_list.html", s=s)
